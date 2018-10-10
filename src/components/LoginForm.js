80d6c2f688f42393cf8efd2bca9dfb2dbfb7ad2d
Kirill <kirill.abramov@mgid.com>
2018-10-10 15:18:30 +0300

    check

---
+++ src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {VERIFY_USER} from '../Events';
+
+export default class LoginForm extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            nickname: "",
+            error:""
+        };
+    }
+
+    setUser = ({user, isUser})=>{
+        console.log(user, isUser);
+        if (isUser) {
+            this.setError("Username already taken");
+        }else{
+            this.setError("");
+            this.props.setUser(user);
+            
+        }
+    }
+
+    setError = error => {
+        this.setState({error});
+    }
+    handleSubmit = (e)=>{
+        e.preventDefault();
+
+        const {socket} = this.props;
+        const {nickname} = this.state;
+        socket.emit(VERIFY_USER, nickname, this.setUser);
+
+    }
+
+    handleChange = (e)=>{
+        this.setState({nickname:e.target.value});
+    }
+
+    render() {
+        const { nickname, error } = this.state;
+        return(
+            <div className="login">
+                <form onSubmit={this.handleSubmit} className="login-form">
+                    <label htmlFor="nickname">
+                        <h2>Write your nickname</h2>
+                    </label>
+                    <input 
+                    ref={(input)=>{this.textInput = input }}
+                    type="text"
+                    id="nickname" 
+                    value={nickname}
+                    onChange={this.handleChange}
+                    placeholder={'Your username'}/>
+
+                    <div className="error">{error ? error:null}</div>
+                </form>
+            </div>
+        );
+    }
+}
